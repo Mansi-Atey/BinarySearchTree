7f66a446e1536e7bf0ed6ef760a02f2e5bce01b0
Mansi-Atey <mansiatey31@gmail.com>
2021-03-26 17:11:16 +0530

    [mansi]ADDED: code to create BST

---
+++ UC1-BST/UC1-BST/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC1_BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST binarySearchTree = new BST();
+            binarySearchTree.Insert(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+        }
+    }
+}
