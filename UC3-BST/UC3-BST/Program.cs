e6b067721275b9601adcfc67a19f9a4a2a76cbea
Mansi-Atey <mansiatey31@gmail.com>
2021-03-28 22:47:12 +0530

    [mansi]ADDED: to search BST

--- UC3-BST/UC3-BST/Program.cs
+++ UC3-BST/UC3-BST/Program.cs
@@ -2,28 +2,26 @@
 
 namespace UC3_BST
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            BST binarySearchTree = new BST();
-            binarySearchTree.Insert(56);
-            binarySearchTree.Insert(30);
-            binarySearchTree.Insert(70);
-            binarySearchTree.Insert(22);
-            binarySearchTree.Insert(40);
-            binarySearchTree.Insert(11);
-            binarySearchTree.Insert(3);
-            binarySearchTree.Insert(16);
-            binarySearchTree.Insert(60);
-            binarySearchTree.Insert(95);
-            binarySearchTree.Insert(65);
-            binarySearchTree.Insert(63);
-            binarySearchTree.Insert(67);
-            binarySearchTree.Display();
-            bool result = binarySearchTree.IfExists(63, binarySearchTree);
-            Console.WriteLine("size of binary search tree is : " + binarySearchTree.Size());
-            Console.WriteLine(result + " found in binary search tree");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            binarySearch.Display();
+            bool result = binarySearch.IfExists(63, binarySearch);
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
